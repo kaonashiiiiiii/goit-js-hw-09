4018c16a9c7695b1abd4bb5a12dd4d1e17a5831d
kaonashiiiiiii <127231288+kaonashiiiiiii@users.noreply.github.com>
2023-08-03 20:00:28 +0000

    Deploying to gh-pages from @ kaonashiiiiiii/goit-js-hw-09@d29663d80605909a994888039d304c5769c9a882 🚀

---
+++ 01-color-switcher.f0d84280.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a;t.addEventListener("click",(function(){a=setInterval((()=>{d.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.f0d84280.js.map
