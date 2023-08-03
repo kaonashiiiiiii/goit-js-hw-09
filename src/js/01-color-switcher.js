d29663d80605909a994888039d304c5769c9a882
kaonashiiiiiii <priora102003@gmail.com>
2023-08-03 23:00:06 +0300

    firsttask

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+btnStart.addEventListener('click', changeBg);
+btnStop.addEventListener('click', stopChange);
+
+let idInrl;
+
+function changeBg() {
+  idInrl = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function stopChange() {
+  clearInterval(idInrl);
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
